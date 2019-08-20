ba9e89578b69df6b69894fe97267b8ce61f8007a
ipa-maes <matthias.schneider2@ipa.fraunhofer.de>
2019-08-20 11:02:16 +0200

    Add source to travis deploy script

--- scripts/deploy-packages.sh
+++ scripts/deploy-packages.sh
@@ -2,5 +2,6 @@
 
 # Nuget
 echo "Pushing packages to Nuget..." &&
-dotnet nuget push src/Fraunhofer.IPA.MSB.Client.API/bin/Release/Fraunhofer.IPA.MSB.Client.API.*.nupkg --api-key $NUGET_API_KEY
-dotnet nuget push src/Fraunhofer.IPA.MSB.Client.Websocket/bin/Release/Fraunhofer.IPA.MSB.Client.Websocket.*.nupkg --api-key $NUGET_API_KEY
+NUGET_SOURCE="https://api.nuget.org/v3/index.json"
+dotnet nuget push src/Fraunhofer.IPA.MSB.Client.API/bin/Release/Fraunhofer.IPA.MSB.Client.API.$TRAVIS_BRANCH.nupkg --source $NUGET_SOURCE --api-key $NUGET_API_KEY
+dotnet nuget push src/Fraunhofer.IPA.MSB.Client.Websocket/bin/Release/Fraunhofer.IPA.MSB.Client.Websocket.$TRAVIS_BRANCH.nupkg --source $NUGET_SOURCE --api-key $NUGET_API_KEY
