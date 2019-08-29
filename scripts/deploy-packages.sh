bede25b31a744bcd6528d78b4955281d1263fda0
ipa-maes <matthias.schneider2@ipa.fraunhofer.de>
2019-08-29 13:23:01 +0200

    Update travis deploy package script to find nuget packages using .nupkg file ending

--- scripts/deploy-packages.sh
+++ scripts/deploy-packages.sh
@@ -3,5 +3,7 @@
 # Nuget
 echo "Pushing packages to Nuget..." &&
 NUGET_SOURCE="https://api.nuget.org/v3/index.json"
-dotnet nuget push src/Fraunhofer.IPA.MSB.Client.API/bin/Release/Fraunhofer.IPA.MSB.Client.API.$TRAVIS_BRANCH.nupkg --source $NUGET_SOURCE --api-key $NUGET_API_KEY
-dotnet nuget push src/Fraunhofer.IPA.MSB.Client.Websocket/bin/Release/Fraunhofer.IPA.MSB.Client.Websocket.$TRAVIS_BRANCH.nupkg --source $NUGET_SOURCE --api-key $NUGET_API_KEY
+NUGET_FILE=$(find src/Fraunhofer.IPA.MSB.Client.API/bin/Release/ -name \*.nupkg)
+dotnet nuget push $NUGET_FILE --source $NUGET_SOURCE --api-key $NUGET_API_KEY
+NUGET_FILE=$(find src/Fraunhofer.IPA.MSB.Client.Websocket/bin/Release/ -name \*.nupkg)
+dotnet nuget push $NUGET_FILE --source $NUGET_SOURCE --api-key $NUGET_API_KEY
